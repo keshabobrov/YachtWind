022f26124e5b0a68de8218a8d3da822c25fd3064
vikentybobrov <keshabobrov@gmail.com>
2023-12-22 13:42:48 +0300

    - Removed legacy code

--- static/main.js
+++ static/main.js
@@ -20,32 +20,6 @@ function ajaxRequest(formData, url) {
 }
 
 
-// function event_request() {
-//     return new Promise((resolve, reject) => {
-//         let formData = null;
-//         let url = "/event_request";
-//         const array = new Map()
-//         ajaxRequest(formData, url).then((value) => {
-//             for (let i = 1; i <= value[0]; i++) {
-//                 let uid_name = "uid_" + i;
-//                 let user_name = "user_" + i;
-//                 let date_name = "date_" + i;
-//                 let time_name = "time_" + i;
-//                 let all_sl_name = "all_sl_" + i;
-//                 let free_sl_name = "free_sl_" + i;
-//                 array.set(uid_name, value[i]);
-//                 array.set(user_name, value[i + value[0]]);
-//                 array.set(date_name, value[i + value[0] * 2]);
-//                 array.set(time_name, value[i + value[0] * 3]);
-//                 array.set(all_sl_name, value[i + value[0] * 4]);
-//                 array.set(free_sl_name, value[i + value[0] * 5]);
-//             }
-//             resolve(array)
-//         });
-//     });
-// }
-
-
 function get_id() {
     let telegram = window.Telegram.WebApp;
         try {
@@ -181,47 +155,41 @@ function showEvents() {
             row.cells[2].style.display = "none";
             row.cells[3].style.display = "none";
         }
+        addRowHandlers()
     })
 }
-    // event_request().then((value) => {
-    //     let table = document.getElementById('enroll_table');
-    //     for (i=1; i <= (value.size / 6); i++) {
-    //         let uid = value.get("uid_" + i);
-    //         let user = value.get("user_" + i);
-    //         user = user.split(' ').slice(0, 2).join(' ');
-    //         let date = value.get("date_" + i);
-    //         let time = value.get("time_" + i);
-    //         let free_sl = value.get("free_sl_" + i);
-    //         let row = table.insertRow(i);
-    //         row.insertCell(0).innerHTML = user;
-    //         row.insertCell(1).innerHTML = time.slice(0, -3);
-    //         row.insertCell(2).innerHTML = free_sl;
-    //         row.insertCell(3).innerHTML = date;
-    //         row.insertCell(4).innerHTML = uid;
-    //         row.className = "rows invisible";
-    //         row.cells[2].style.display = "none";
-    //         row.cells[3].style.display = "none";
-    //         row.cells[4].style.display = "none";
-    //         let cell_trainer = row.cells[0];
-    //         let cell_time = row.cells[1];
-    //         cell_time.className = "number_blue_box";
-    //         cell_trainer.className = "trainer_td";
-    //         if (date === value.get("date_1")) {
-    //             row.className = "rows";
-    //             let date = new Date(value.get("date_1"))
-    //             document.getElementById("time_stamp").innerHTML = value.get("date_1")
-    //             document.getElementById("date_picker_text").innerHTML = date.toLocaleString("ru", {
-    //                 month: "long",
-    //                 day: "numeric"
-    //             })
-    //         }
-    //     }
-    //     /** добавление обработчика нажатий на строки таблицы*/
-    //     addRowHandlers()
-    // })
-// }
 
-function updateVisiblity(table, date_vis, dates, newIndex) {
+
+function listHandler(direction) {
+    const table = document.getElementById("enroll_table");
+    const date_vis = document.getElementById("time_stamp").innerHTML;
+    let dates = []
+    for (let i = 1; i < table.rows.length; i++) {
+        dates.push(table.rows[i].cells[3].innerHTML);
+    }
+    const currentIndex = dates.indexOf(date_vis);
+    if (direction === 0) {
+        let newIndex = currentIndex - 1;
+        while (newIndex >= 0) {
+            let res = updateVisibility(table, date_vis, dates, newIndex)
+            if (res === 0) {
+                return;
+            }
+            newIndex += 1;
+        }
+    }
+    if (direction === 1) {
+        let newIndex = currentIndex + 1;
+        while (newIndex < dates.length) {
+            let res = updateVisibility(table, date_vis, dates, newIndex)
+            if (res === 0) {return;}
+            newIndex += 1;
+        }
+    }
+}
+
+
+function updateVisibility(table, date_vis, dates, newIndex) {
     if (dates[newIndex] !== date_vis) {
         const date = new Date(dates[newIndex])
         document.getElementById("date_picker_text").innerHTML = date.toLocaleString("ru", {
@@ -238,34 +206,6 @@ function updateVisiblity(table, date_vis, dates, newIndex) {
             }
         }
         return 0;
-    }
-}
-
-function listHandler(direction) {
-    const table = document.getElementById("enroll_table");
-    const date_vis = document.getElementById("time_stamp").innerHTML;
-    let dates = []
-    for (let i = 1; i < table.rows.length; i++) {
-        dates.push(table.rows[i].cells[3].innerHTML);
-    }
-    const currentIndex = dates.indexOf(date_vis);
-    if (direction === 0) {
-        let newIndex = currentIndex - 1;
-        while (newIndex >= 0) {
-            let res = updateVisiblity(table, date_vis, dates, newIndex)
-            if (res === 0) {
-                return;
-            }
-            newIndex += 1;
-        }
-    }
-    if (direction === 1) {
-        let newIndex = currentIndex + 1;
-        while (newIndex < dates.length) {
-            let res = updateVisiblity(table, date_vis, dates, newIndex)
-            if (res === 0) {return;}
-            newIndex += 1;
-        }
     }
 }
 
